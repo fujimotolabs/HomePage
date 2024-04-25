84cc7782533dfdf7f26120e2d12265b226d05d9b
ei-sugimoto <eikun1014@gmail.com>
2024-04-25 16:25:39 +0000

    メソッド修正

--- my-vue-app/src/router.ts
+++ my-vue-app/src/router.ts
@@ -1,4 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
 const routes = [
   {
     path: "/",
@@ -14,6 +18,6 @@ const routes = [
   },
 ];
 export const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
