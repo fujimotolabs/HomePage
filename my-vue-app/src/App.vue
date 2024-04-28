28138908c7746c4f6efe355a992d46b74677b84c
ei-sugimoto <eikun1014@gmail.com>
2024-04-28 08:12:18 +0000

    vuetifyインストール

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -3,20 +3,19 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <RouterView />
-</template>
+  <v-app>
+    <v-layout class="rounded rounded-md">
+      <v-app-bar title="Application bar"></v-app-bar>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+      <v-navigation-drawer>
+        <v-list>
+          <v-list-item title="Navigation drawer"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <RouterView />
+      </v-main>
+    </v-layout>
+  </v-app>
+</template>
