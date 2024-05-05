d7dd4fa6be4b54ba0974a0e5f405a7dd468469f9
ei-sugimoto <eikun1014@gmail.com>
2024-05-05 12:53:47 +0000

    ホーム画面の追記

---
+++ my-vue-app/src/components/homecomponents/ResearchWord.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { RouterLink } from "vue-router";
+
+defineProps<{
+  renderingAbout: boolean;
+}>();
+</script>
+
+<template>
+  <div>
+    <v-sheet
+      elevation="16"
+      class="w-full h-full mb-16"
+      :class="{
+        'animate__animated animate__fadeInLeftBig': renderingAbout,
+      }"
+    >
+      <div class="font-bold text-3xl text-teal-700 m-10">Research Word</div>
+      <v-expansion-panels multiple>
+        <v-expansion-panel>
+          <v-expansion-panel-title>
+            <div class="font-bold text-xl">高性能計算</div>
+            <template #actions="{ expanded }">
+              <FontAwesomeIcon :icon="expanded ? faMinus : faPlus" />
+            </template>
+          </v-expansion-panel-title>
+          <v-expansion-panel-text class="font-sans text-slate-600">
+            GPUは多数のコアで構成されており, データ構造とアルゴリズムを工夫し,
+            それらのコアにうまく処理を割り振ると様々なアルゴリズムが高速に実行できます．
+            並列処理による高速化が容易な問題もあれば,
+            とても難しい問題もあります．
+            様々な問題を並列処理で高速に解く方法（並列アルゴリズム）の研究や,
+            実際に高速に動作する並列プログラムを開発するための研究を行っています．
+          </v-expansion-panel-text>
+          <v-expansion-panel-text>
+            <RouterLink to="/" class="text-blue-500"
+              >研究について詳しく
+            </RouterLink>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-title expand-icon="mdi-menu-down">
+            <div class="font-bold text-xl">モバイルコンピューティング</div>
+            <template #actions="{ expanded }">
+              <FontAwesomeIcon :icon="expanded ? faMinus : faPlus" />
+            </template>
+          </v-expansion-panel-title>
+          <v-expansion-panel-text class="font-sans text-slate-600">
+            スマートフォンに代表されるモバイルデバイスの普及率が高まってきている現在，各デバイスがその場の情報をセンシングしながら相互接続することにより，
+            様々な高度なサービスが期待されています．
+            これを実現するためのセンシングや通信に関する様々な研究を行っています．
+          </v-expansion-panel-text>
+          <v-expansion-panel-text>
+            <RouterLink to="/" class="text-blue-500"
+              >研究について詳しく
+            </RouterLink>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-sheet>
+  </div>
+</template>
--- my-vue-app/src/views/HomeContainer.vue
+++ my-vue-app/src/views/HomeContainer.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import AboutUs from "@/components/homecomponents/AboutUs.vue";
+import HomeImage from "@/components/homecomponents/HomeImage.vue";
+import ResearchWord from "@/components/homecomponents/ResearchWord.vue";
+
 import { ref } from "vue";
 
 const renderingAbout = ref(false);
@@ -10,32 +14,9 @@ setTimeout(() => {
 
 <template>
   <div v-if="renderingAbout" class="flex flex-col gap-10">
-    <v-sheet
-      class="w-full h-full p-10"
-      :class="{
-        'animate__animated animate__fadeInLeftBig': renderingAbout,
-      }"
-      elevation="16"
-    >
-      <div class="font-bold text-5xl mb-10 text-blue-900">About Us</div>
-      <p class="text-xl text-gray-500 font-sans">
-        当研究室は大阪公立大学の情報学研究科・基幹情報学専攻・システム情報学分野/工学部・情報工学科に所属しています．
-        前身は理学系研究科情報数理科学専攻（当時名称）の藤本研究室と勝間研究室であり,
-        2016年度から工学研究科知能情報工学分野の第5グループ研究室として発足しました.
-        多数の計算コアを持つGPUやマルチコアプロセッサなどの並列計算環境を利用した様々なアルゴリズムの高速化や,
-        モバイル機器の通信機能やセンサを利用した高度なサービスを実現するための技術についての研究を行っています.
-      </p>
-    </v-sheet>
-    <div
-      class="flex justify-center"
-      :class="{
-        'animate__animated animate__fadeInRightBig': renderingAbout,
-      }"
-    >
-      <v-sheet elevation="16" class="w-max h-full">
-        <img src="../../public/lab5-01.png" class="scale-100" />
-      </v-sheet>
-    </div>
+    <AboutUs :rendering-about="renderingAbout" />
+    <HomeImage :rendering-about="renderingAbout" />
+    <ResearchWord :rendering-about="renderingAbout" />
   </div>
 </template>
 
