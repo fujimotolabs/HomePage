4bb7b7337f52e7bbe842790021b994ea19d16f6a
ei-sugimoto <143775446+ei-sugimoto@users.noreply.github.com>
2024-05-08 14:21:26 +0900

    Feature/create home container (#32) (#38)
    * ホーム画面を追記
    
    * ホーム画面の追記

---
+++ my-vue-app/src/components/homecomponents/HomeImage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  renderingAbout: boolean;
+}>();
+</script>
+
+<template>
+  <div>
+    <div
+      class="flex justify-center"
+      :class="{
+        'animate__animated animate__fadeInRightBig': renderingAbout,
+      }"
+    >
+      <v-sheet elevation="16" class="w-max h-full">
+        <img src="../../../public/lab5-01.png" class="scale-100" />
+      </v-sheet>
+    </div>
+  </div>
+</template>
