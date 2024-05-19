f3e547dd67774a1b01f7af8df6826871b444b918
ei-sugimoto <eikun1014@gmail.com>
2024-05-19 14:57:33 +0000

    月表示を修正

--- my-vue-app/src/components/homecomponents/NewsBox.vue
+++ my-vue-app/src/components/homecomponents/NewsBox.vue
@@ -3,7 +3,7 @@ import { someNews } from "@/data/NewsData";
 
 const decodeDateToString = (date: Date) => {
   const year = date.getFullYear();
-  const month = date.getMonth();
+  const month = date.getMonth() + 1;
   const day = date.getDate();
   return `${year}/${month}/${day}`;
 };
